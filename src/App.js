a220aa3df3f1ab01b324fc7b87bb658b5e99d553
qqwrxtr <teodorghirba1@gmail.com>
2024-08-12 14:16:14 +0300

    testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
+import React, { useState } from 'react';
 import './App.css';
 import Banner from './components/Banner/Banner';
 import Features from './components/Features/Features';
 import Header from './components/Header/Header';
 import InfoBlock from './components/InfoAbt/Infoabt';
 import Offers from './components/Offers/Offers';
+import Preloader from './components/Preloader/Preloader';
 
 function App() {
+  const [imagesLoaded, setImagesLoaded] = useState(false);
+
+  const handleImagesLoaded = () => {
+    setImagesLoaded(true);
+  };
+
   return (
-    <div className="app d-flex flex-column justify-content-center">
-      <header>
-        <Header/>
-      </header>
-      <main className='d-flex flex-column justify-content-center align-items-center'>
-        <Banner/>
-        <InfoBlock/>
-        <Offers/>
-        <Features/>
-      </main>
-    </div>
+    <>
+      {!imagesLoaded && <Preloader />}
+      {imagesLoaded && (
+        <div className="app d-flex flex-column justify-content-center">
+          <header>
+            <Header />
+          </header>
+          <main className='d-flex flex-column justify-content-center align-items-center'>
+            <Banner onImagesLoaded={handleImagesLoaded} />
+            <InfoBlock />
+            <Offers />
+            <Features />
+          </main>
+        </div>
+      )}
+    </>
   );
 }
 
