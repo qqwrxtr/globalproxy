a9aaada17a3f853bb8721ae2a61353de5b676c71
qqwrxtr <teodorghirba1@gmail.com>
2024-08-13 17:08:17 +0300

    done with adaptation for all blocks

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,36 +1,63 @@
 import { useTranslation } from "react-i18next";
-import "./footer.css"
+import "./footer.css";
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
 
 const Footer = () => {
-
     const { t } = useTranslation();
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: true, threshold: 0.1 });
 
-    return ( 
-        <div className="container container_footer d-flex justify-content-center">
+    const containerVariants = {
+        hidden: { opacity: 0, y: 0 },
+        visible: {
+            opacity: 1,
+            y: 0,
+            transition: {
+                duration: 0.3,
+                when: "beforeChildren",
+                staggerChildren: 0.3,
+            },
+        },
+    };
+
+    const itemVariants = {
+        hidden: { opacity: 0, y: 50 },
+        visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+    };
+
+    return (
+        <motion.div
+            ref={ref}
+            initial="hidden"
+            animate={isInView ? "visible" : "hidden"}
+            variants={containerVariants}
+            className="container container_footer d-flex justify-content-center"
+        >
             <div className="row d-flex align-items-center justify-content-center row_content_footer">
                 <div className="col-12 col-md-6 text-md-start text-center">
-                    <div className="part_one_footer_text">
+                    <motion.div className="part_one_footer_text" variants={itemVariants}>
                         <div className="title_footer_part_one title_footer">
                             <p>proxy-lab.com</p>
                         </div>
                         <div className="subtitle_footer_part_one subtitle_footer">
                             <p>{t("SubtitleFooterPartOne")}</p>
                         </div>
-                    </div>
+                    </motion.div>
                 </div>
                 <div className="col-12 col-md-6 text-md-start text-center">
-                    <div className="part_two_footer_text">
+                    <motion.div className="part_two_footer_text" variants={itemVariants}>
                         <div className="title_footer_part_two title_footer">
                             <p>@ProxyLab</p>
                         </div>
                         <div className="subtitle_footer_part_two subtitle_footer">
                             <p>{t("SubtitleFooterPartTwo")}</p>
                         </div>
-                    </div>
+                    </motion.div>
                 </div>
             </div>
-        </div>
-     );
-}
- 
+        </motion.div>
+    );
+};
+
 export default Footer;
--- src/components/TechSupport/Techsupport.jsx
+++ src/components/TechSupport/Techsupport.jsx
@@ -1,56 +1,93 @@
 import { useTranslation } from "react-i18next";
-import "./techsupport.css"
+import "./techsupport.css";
 import Subpoints from "../Subpoints/Subpoints";
-import globus from "./../../assets/img/globus.svg"
-
+import globus from "./../../assets/img/globus.svg";
+import { titleAnimationVariants } from "./../../Utils/TitleAnimation/TitleAnimation.jsx";
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
 
 const TechSupport = () => {
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: true, threshold: 0.2 });
 
-    const {t} = useTranslation();
+    const { t } = useTranslation();
 
     const subpoints = [
-        {text:t("TextTech1")},
-        {text:t("TextTech2")},
-        {text:t("TextTech3")}
-    ]
+        { text: t("TextTech1") },
+        { text: t("TextTech2") },
+        { text: t("TextTech3") }
+    ];
 
+    const containerVariants = {
+        hidden: { opacity: 0, y: 50 },
+        visible: {
+            opacity: 1,
+            y: 0,
+            transition: {
+                duration: 0.2,
+                when: "beforeChildren",
+                staggerChildren: 0.2,
+            },
+        },
+    };
 
-    return ( 
-        <div className="container container_tech d-flex flex-column justify-content-center" id="support">
+    const itemVariants = {
+        hidden: { opacity: 0, y: 50 },
+        visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+    };
+
+    return (
+        <motion.div
+            ref={ref}
+            initial="hidden"
+            animate={isInView ? "visible" : "hidden"}
+            variants={containerVariants}
+            id="support"
+            className="container container_tech d-flex flex-column justify-content-center"
+        >
             <div className="row w-100">
-                <div className="col-12 tech_title d-flex justify-content-center align-items-center ">
-                    <p>{t("Tech_Support")}</p>
+                <div className="col-12 tech_title d-flex justify-content-center align-items-center">
+                    <motion.div
+                        initial={titleAnimationVariants.initial}
+                        animate={isInView ? titleAnimationVariants.animate : titleAnimationVariants.initial}
+                        transition={titleAnimationVariants.transition}
+                    >
+                        <p>{t("Tech_Support")}</p>
+                    </motion.div>
                 </div>
             </div>
-            <div className="row d-flex justify-content-center for_wrap_tech">
+            <motion.div
+                className="row d-flex justify-content-center for_wrap_tech"
+                variants={containerVariants}
+            >
                 <div className="col-md-4 col-12 col_gap_tech">
-                    <div className="text_part_tech">
+                    <motion.div className="text_part_tech" variants={itemVariants}>
                         <div className="title_tech">
                             <p>{t("TitleTech")}</p>
                         </div>
                         <div className="subtitle_tech">
                             <p>{t("SubtitleTech")}</p>
                         </div>
-                    </div>
-                    <div className="subpoints_container">
+                    </motion.div>
+                    <motion.div className="subpoints_container" variants={containerVariants}>
                         {subpoints.map((points, index) => (
-                            <div className="subpoints" key={index}>
-                                <Subpoints text={points.text} variant="tech"/>
-                            </div>
+                            <motion.div className="subpoints" key={index} variants={itemVariants}>
+                                <Subpoints text={points.text} variant="tech" />
+                            </motion.div>
                         ))}
-                    </div>
-                    <div className="contact_us_button_tech">
+                    </motion.div>
+                    <motion.div className="contact_us_button_tech" variants={itemVariants}>
                         <a href="">
                             <button>{t("Contact_Us")}</button>
                         </a>
-                    </div>
+                    </motion.div>
                 </div>
-                <div className="col-md-4 col-12 img_tech d-flex align-items-center justify-content-end">
-                        <img src={globus} alt="" className="img-fluid"/>
-                </div>
-            </div>
-        </div>
-     );
-}
- 
+                <motion.div className="col-md-4 col-12 img_tech d-flex align-items-center justify-content-lg-end justify-content-center" variants={itemVariants}>
+                    <img src={globus} alt="" className="img-fluid" />
+                </motion.div>
+            </motion.div>
+        </motion.div>
+    );
+};
+
 export default TechSupport;
